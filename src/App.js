34d4bf14d0bec5c8b5adee4471cb1c2244d77227
Erick PC <erksters@gmail.com>
2020-11-24 15:43:57 -0600

    added routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+
+const  App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>This is a nav bar</div>
+      <Router>
+        <Switch>
+          <Route path="/create/tenant">
+            <div>create tenant</div>
+          </Route>
+          <Route path="/update/tenant">
+          <div>update tenant</div>
+          </Route>
+          <Route path="/create/apartment">
+          <div>create apartment</div>
+          </Route>
+          <Route path="/update/apartment">
+          <div>update apartment</div>
+          </Route>
+          <Route path="/create/contract">
+          <div>create contract</div>
+          </Route>
+          <Route exact path="/">
+          <div>landing page</div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
